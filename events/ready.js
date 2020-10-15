f7f8579ab9b9310ae0c8d27afefd671d8ee4ba38
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-15 15:32:29 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity(`f!help || Watching ${client.guilds.cache.size} Servers And ${client.users.cache.size} Members`, {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
