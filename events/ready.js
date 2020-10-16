bd167b99dee948b3a61e8b173d791f91678e51a6
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-16 13:29:01 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
+  setInterval(() => {
   await client.user.setActivity(`f!help || Watching ${client.guilds.cache.size} Servers And ${client.users.cache.size} Members`, {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING  
   });
+}, 300000);
 };
