d748664ef1a9724c88af5daa2aba43afa727767c
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-17 15:00:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ client.queue = new Map()
 client.config = {
   prefix: "f!"
 }
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
 
 //Loading Events
 fs.readdir(__dirname + "/events/", (err, files) => {
