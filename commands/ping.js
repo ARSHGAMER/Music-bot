617fb3bba85fd16518b643eb25ffef33d8a59a07
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-19 17:18:37 +0530

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Ping Of The Bot",
-  execute(client, message) {
-    message.channel.send(`Pong :) 🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-  }
-};
