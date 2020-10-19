715ad84b26f1f6c2f5fef4d64ba6df71c41afc06
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-19 17:11:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ client.on("message", message => {
    if (message.author.bot) return;
   if (!message.guild) return;
   
-  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+  if(message.content.startsWith("f!")) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
     
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const args = message.content.slice("f!".length).trim().split(/ +/) //removing prefix from args
     const command = args.shift().toLowerCase();
     
     if(!client.commands.has(command)) {
