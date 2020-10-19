f29067d8fef81c45268a324fbe388bc45647b66e
ARSHGAMER <65659216+ARSHGAMER@users.noreply.github.com>
2020-10-19 17:08:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,5 +48,34 @@ client.on('message', message => {
   }
 });
 
+
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    //COMMAND LOGS
+    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+  
+});
+
+
 //Logging in to discord
 client.login("NzY1OTAzNjE0MDE0OTE0NTcw.X4blUw._Hrazo_gDji4cMGa3vbF0XCD_7g");
